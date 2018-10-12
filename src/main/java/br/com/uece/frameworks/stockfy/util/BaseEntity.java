4628aaaf64bcb3c3e8a9e0c81e2605738ccb5ab3
Marcos Davy Guedes Dias <m.davy.br@gmail.com>
2018-10-12 01:19:37 -0300

    campos createDate e mofiedData no BaseEntity

--- src/main/java/br/com/uece/frameworks/stockfy/util/BaseEntity.java
+++ src/main/java/br/com/uece/frameworks/stockfy/util/BaseEntity.java
@@ -5,12 +5,19 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.io.Serializable;
+import java.time.Instant;
 
 /**
  * Create by Bruno Barbosa - 11/10/2018
  */
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @SuppressWarnings("serial")
 @Getter
 @Setter
@@ -21,4 +28,10 @@ public class BaseEntity <PK extends Serializable> implements Serializable {
     @Basic(optional = false)
     @Column(name = "id", nullable = false)
     private PK id;
+
+    @CreatedDate
+    private Instant createDate;
+
+    @LastModifiedDate
+    private Instant modifiedDate;
 }
