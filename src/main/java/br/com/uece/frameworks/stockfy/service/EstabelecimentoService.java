91a4b64993626110982a4b8275ad23c65951b054
Bruno Barbosa <barbosa.developer@gmail.com>
2018-11-15 17:54:49 -0300

    Estabelecimento CRUD

---
+++ src/main/java/br/com/uece/frameworks/stockfy/service/EstabelecimentoService.java
@@ -0,0 +1,11 @@
+package br.com.uece.frameworks.stockfy.service;
+
+import br.com.uece.frameworks.stockfy.model.Estabelecimento;
+import org.springframework.stereotype.Service;
+
+/**
+ * Create by Bruno Barbosa - 15/11/2018
+ */
+@Service
+public class EstabelecimentoService extends GenericService<Estabelecimento> {
+}
